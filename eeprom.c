b2757fdc008c27d1ffaefed17cc4b68f4e62aba4
skusiak <kakao131.com@gmail.com>
2025-01-23 19:29:15 +0100

    created files for eeprom code

---
+++ eeprom.c
@@ -0,0 +1,10 @@
+/**
+ * @file eeprom.c
+ * @author Stanislaw Kusiak
+ * @date winter 2024/2025 semester
+ * @brief File function definitions for eeprom.
+ * @ver 1.0
+ */
+ 
+#include "eeprom.h"
+
---
+++ eeprom.h
@@ -0,0 +1,16 @@
+/**
+ * @file eeprom.h
+ * @author Stanislaw Kusiak
+ * @date winter 2024/2025 semester
+ * @brief File function declaration for eeprom.
+ * @ver 1.0
+ */
+
+#include "i2c.h"
+
+#ifndef EEPROM_H
+#define EEPROM_H
+
+
+
+#endif
