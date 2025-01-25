5d602daebdb25e351100715177e57f45dd8fbc97
skusiak <117866285+kakao16@users.noreply.github.com>
2025-01-25 14:17:20 +0100

    Create function prototyes for eeprom library.

--- eeprom.c
+++ eeprom.c
@@ -8,3 +8,15 @@
  
 #include "eeprom.h"
 
+uint8_t EEPROM_Init(void) {
+	I2C_Init();
+	return 0;
+}
+
+uint8_t EEPROM_Write_f(uint8_t address, float value) {
+	return 1;
+}
+
+uint8_t EEPROM_Read_f(uint8_t address, float *value) {
+	return 1;
+}
--- eeprom.h
+++ eeprom.h
@@ -11,6 +11,26 @@
 #ifndef EEPROM_H
 #define EEPROM_H
 
+/**
+ * @brief Write float value to eeprom.
+ * @return Errors.
+ */
+uint8_t EEPROM_Init(void);
 
+/**
+ * @brief Write float value to eeprom.
+ * @param Memory address.
+ * @param Value to write.
+ * @return Errors.
+ */
+uint8_t EEPROM_Write_f(uint8_t address, float value);
+
+/**
+ * @brief Read float value from eeprom.
+ * @param Memory address.
+ * @param Variable to write to.
+ * @return Errors.
+ */
+uint8_t EEPROM_Read_f(uint8_t address, float *value);
 
 #endif
